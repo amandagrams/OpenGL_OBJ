bde57d293232db1dd69e419640c3386b0dd26356
Amanda <amanda.grams@gmail.com>
2018-10-06 22:30:45 -0300

    Revert "FIXBUG - Camera movimento distorcido."
    This reverts commit ee1d177cdde486546edab19ec884548d9732fdeb.

--- Camera.h
+++ Camera.h
@@ -14,16 +14,15 @@ public:
 	glm::mat4 getViewMatrix() const;
 
 	virtual void setPosition(const glm::vec3& position) {}
-	virtual void rotate(float yaw, float pitch) {}  // in degrees
+	virtual void rotate(float yaw, float pitch) {}  // em graus
 	virtual void move(const glm::vec3& offsetPos) {}
 
 	const glm::vec3& getLook() const;
 	const glm::vec3& getRight() const;
 	const glm::vec3& getUp() const;
-	const glm::vec3& getPosition() const;
 
-	float getFOV() const { return mFOV; }
-	void setFOV(float fov) { mFOV = fov; }		// in degrees
+	float getFOV() const   { return mFOV; }
+	void setFOV(float fov) { mFOV = fov; }		// em graus
 
 protected:
 	Camera();
@@ -42,7 +41,7 @@ protected:
 	float mPitch;
 
 	// Camera parameters
-	float mFOV; // degrees
+	float mFOV; // em graus
 };
 
 //--------------------------------------------------------------
@@ -52,10 +51,10 @@ class FPSCamera : public Camera
 {
 public:
 
-	FPSCamera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), float yaw = glm::pi<float>(), float pitch = 0.0f); // (yaw) initial angle faces -Z
+	FPSCamera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), float yaw = glm::pi<float>(), float pitch = 0.0f); // (yaw) faces angulares iniciais -Z
 
 	virtual void setPosition(const glm::vec3& position);
-	virtual void rotate(float yaw, float pitch);	// in degrees
+	virtual void rotate(float yaw, float pitch);	// em graus
 	virtual void move(const glm::vec3& offsetPos);
 
 private:
@@ -73,7 +72,7 @@ public:
 
 	OrbitCamera();
 
-	virtual void rotate(float yaw, float pitch);    // in degrees
+	virtual void rotate(float yaw, float pitch);    // em graus
 
 	// Camera Controls
 	void setLookAt(const glm::vec3& target);
@@ -87,4 +86,3 @@ private:
 	float mRadius;
 };
 #endif //CAMERA_H
-
